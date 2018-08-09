75898ecc413d830b04c9e0c82e352fdf3d2da641
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2018-08-09 20:06:08 +0200

    Introduce the lifecycle listener class
    for now, only override the jndi names for the transaction manager and the
    transaction synchronization registry.

---
+++ tomcat/tomcat-jta/src/main/java/org/jboss/narayana/tomcat/jta/NarayanaLifecycleListener.java
@@ -0,0 +1,33 @@
+package org.jboss.narayana.tomcat.jta;
+
+import java.util.logging.Logger;
+
+import com.arjuna.ats.jta.common.jtaPropertyManager;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+
+// implementation inspired from
+
+public class NarayanaLifecycleListener implements LifecycleListener {
+
+    private static Logger LOG = Logger.getLogger(NarayanaLifecycleListener.class.getName());
+
+    public void lifecycleEvent(LifecycleEvent event) {
+        if (Lifecycle.BEFORE_INIT_EVENT.equals(event.getType())) {
+            LOG.info("Responding to a 'before init' event");
+            LOG.info("Overriding default JNDI name to match regular Tomcat names");
+            //jtaPropertyManager.getJTAEnvironmentBean().setUserTransactionJNDIContext("java:comp/UserTransaction");
+            jtaPropertyManager.getJTAEnvironmentBean().setTransactionManagerJNDIContext("java:comp/TransactionManager");
+            jtaPropertyManager.getJTAEnvironmentBean().setTransactionSynchronizationRegistryJNDIContext("java:comp/TransactionSynchronizationRegistry");
+        } else if (Lifecycle.BEFORE_START_EVENT.equals(event.getType())) {
+            LOG.info("Responding to a 'before start' event");
+            LOG.info("Initializing Transaction Manager and Synchronization Registry");
+            jtaPropertyManager.getJTAEnvironmentBean().getTransactionManager();
+            jtaPropertyManager.getJTAEnvironmentBean().getTransactionSynchronizationRegistry();
+        }
+
+    }
+
+}
